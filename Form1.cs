cf37ad6dcc11e03de0a52f9b6495cd26242b4690
Andrew Pipo <apipo@atechtraining.com>
2023-05-02 11:43:34 -0400

    Added Functionality for Git Status

--- Form1.cs
+++ Form1.cs
@@ -14,17 +14,25 @@ public partial class Form1 : Form
 
         Button btnGitLog = new Button();
         btnGitLog.Text = "Execute Git Log Command";
-        btnGitLog.Location = new Point(0, 100);
-        btnGitLog.Width = 250;
+        btnGitLog.Location = new Point(0, 25);
+        btnGitLog.Width = 200;
         btnGitLog.Click += btnGitLog_Click;
         this.Controls.Add(btnGitLog);
 
         Button btnGitBranch = new Button();
         btnGitBranch.Text = "Execute Git Branch Command";
-        btnGitBranch.Location = new Point(0, 150);
-        btnGitBranch.Width = 250;
+        btnGitBranch.Location = new Point(0, 50);
+        btnGitBranch.Width = 200;
         btnGitBranch.Click += btnGitBranch_Click;
         this.Controls.Add(btnGitBranch);
+
+        Button btnGitStatus = new Button();
+        btnGitStatus.Text = "Execute Git Status Command";
+        btnGitStatus.Location = new Point(0, 75);
+        btnGitStatus.Width = 200;
+        btnGitStatus.Click += btnGitStatus_Click;
+        this.Controls.Add(btnGitStatus);
+
 
     }
 
@@ -76,6 +84,39 @@ public partial class Form1 : Form
                 {
                     FileName = "git",
                     Arguments = "branch",
+                    WorkingDirectory = workingDirectory,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                using (var process = new Process())
+                {
+                    process.StartInfo = processStartInfo;
+                    process.Start();
+
+                    var output = process.StandardOutput.ReadToEnd();
+                    MessageBox.Show(output);
+                }
+            }
+        }
+    }
+
+    private void btnGitStatus_Click(object? sender, EventArgs e)
+    {
+        using (var folderBrowserDialog = new FolderBrowserDialog())
+        {
+            folderBrowserDialog.Description = "Select the working directory for Git command execution.";
+
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                var workingDirectory = folderBrowserDialog.SelectedPath;
+
+                // Run the Git command using the selected working directory
+                var processStartInfo = new ProcessStartInfo
+                {
+                    FileName = "git",
+                    Arguments = "status",
                     WorkingDirectory = workingDirectory,
                     RedirectStandardOutput = true,
                     UseShellExecute = false,
