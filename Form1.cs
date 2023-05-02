b2b1b363f0b742209dfc8e6f9cc000a21f2bdbb2
Andrew Pipo <apipo@atechtraining.com>
2023-05-02 11:35:03 -0400

    Adding initial files as reference

---
+++ Form1.cs
@@ -0,0 +1,97 @@
+using System.Diagnostics;
+
+namespace GitFromC_;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+
+        this.MinimizeBox = false;
+        this.MaximizeBox = false;
+        this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+        Button btnGitLog = new Button();
+        btnGitLog.Text = "Execute Git Log Command";
+        btnGitLog.Location = new Point(0, 100);
+        btnGitLog.Width = 250;
+        btnGitLog.Click += btnGitLog_Click;
+        this.Controls.Add(btnGitLog);
+
+        Button btnGitBranch = new Button();
+        btnGitBranch.Text = "Execute Git Branch Command";
+        btnGitBranch.Location = new Point(0, 150);
+        btnGitBranch.Width = 250;
+        btnGitBranch.Click += btnGitBranch_Click;
+        this.Controls.Add(btnGitBranch);
+
+    }
+
+    private void btnGitLog_Click(object? sender, EventArgs e)
+    {
+        using (var folderBrowserDialog = new FolderBrowserDialog())
+        {
+            folderBrowserDialog.Description = "Select the working directory for Git command execution.";
+
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                var workingDirectory = folderBrowserDialog.SelectedPath;
+
+                // Run the Git command using the selected working directory
+                var processStartInfo = new ProcessStartInfo
+                {
+                    FileName = "git",
+                    Arguments = "log",
+                    WorkingDirectory = workingDirectory,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                using (var process = new Process())
+                {
+                    process.StartInfo = processStartInfo;
+                    process.Start();
+
+                    var output = process.StandardOutput.ReadToEnd();
+                    MessageBox.Show(output);
+                }
+            }
+        }
+    }
+
+    private void btnGitBranch_Click(object? sender, EventArgs e)
+    {
+        using (var folderBrowserDialog = new FolderBrowserDialog())
+        {
+            folderBrowserDialog.Description = "Select the working directory for Git command execution.";
+
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                var workingDirectory = folderBrowserDialog.SelectedPath;
+
+                // Run the Git command using the selected working directory
+                var processStartInfo = new ProcessStartInfo
+                {
+                    FileName = "git",
+                    Arguments = "branch",
+                    WorkingDirectory = workingDirectory,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                using (var process = new Process())
+                {
+                    process.StartInfo = processStartInfo;
+                    process.Start();
+
+                    var output = process.StandardOutput.ReadToEnd();
+                    MessageBox.Show(output);
+                }
+            }
+        }
+    }
+
+}
